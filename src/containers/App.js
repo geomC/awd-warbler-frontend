66c4f64623d940f0a1270ca748d51bad75a8454f
Sascha <sascha.picard@googlemail.com>
2018-11-10 19:54:55 +0100

    set up redux store

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {Provider}from 'react-redux';
+import {configureStore} from "../store";
+import {BrowserRouter} from 'react-router-dom'
+
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        Hello World
+      </div>
+    </BrowserRouter>
+  </Provider>
+);
+
+export default App;
