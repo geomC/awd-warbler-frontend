1ea7aeb179a83e0f7a3759021bd8939efedb6ba7
Sascha <sascha.picard@googlemail.com>
2018-11-10 20:54:02 +0100

    add navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Provider}from 'react-redux';
 import {configureStore} from "../store";
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
+import NavBar from './Navbar'
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <div>
-        Hello World
+      <div className="onboarding">
+          <NavBar/>
       </div>
     </BrowserRouter>
   </Provider>
